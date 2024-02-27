d617a3d01e491d33315d0d42844efdd6d95712ef
Rakesh-5199 <kr65921@gmail.com>
2024-02-27 19:46:04 +0530

    Call back task

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var h1 = document.querySelector("h1");
+    setTimeout(function() {
+      h1.innerText = "Counter: 10";
+    }, 1000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 9";
+    }, 2000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 8";
+    }, 3000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 7";
+    }, 4000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 6";
+    }, 5000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 5";
+    }, 6000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 4";
+    }, 7000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 3";
+    }, 8000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 2";
+    }, 9000);
+  
+    setTimeout(function() {
+      h1.innerText = "Counter: 1";
+    }, 10000);
+  
+    setTimeout(function() {
+      h1.innerText = "Happy Independence Day!";
+    }, 11000);
+  });
+  
